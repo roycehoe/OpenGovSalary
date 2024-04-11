127dc73670dc3e90078f299d101ae5cd046fe873
roycehoe <royceinbox@hotmail.com>
2024-04-11 15:32:42 +0800

    feat: Create function to generate markdown of staff salaries for README.md file

---
+++ generate_markdown.py
@@ -0,0 +1,14 @@
+import json
+
+from models import StaffResponse
+
+with open("data.json") as file:
+    loaded_file = json.load(file)
+    all_staff_data = [StaffResponse(**i) for i in loaded_file]
+    sorted_all_staff_data = sorted(
+        all_staff_data, key=lambda staff: staff.salary, reverse=True
+    )
+    print("| Name | Title | Salary |")
+    print("| ---- | ---- | ---- |")
+    for staff in sorted_all_staff_data:
+        print(f"|{staff.name} | {staff.title} | ${staff.salary:,.0f}|")
