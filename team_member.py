39ebaa139dca017922e61b72b2d9e55c27c509d2
Royce <royceinbox@hotmail.com>
2024-08-26 18:28:36 +0800

    chore: obtain staff info from staff page

--- team_member.py
+++ team_member.py
@@ -1,14 +1,15 @@
+import re
 from dataclasses import dataclass
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 PLACEHOLDER_URL = "https://www.open.gov.sg/people/charmaine"
 
 
 @dataclass
 class StaffMember:
-    pic: str
+    profile_picture: str
     name: str
     title: str
     join_date: str
@@ -24,30 +25,18 @@ def get_ogp_api_people_info_response(
         raise Exception  # To handle custom error here
 
 
-def get_staff_info(ogp_api_people_info_response: str):
+def get_staff_info(ogp_api_people_info_response: str) -> StaffMember:
     soup = BeautifulSoup(ogp_api_people_info_response, features="html.parser")
-    test = soup.find("div", {"class": "staff-pic"})
-    # staff_main_info = soup.find("div", {"class": "staff-main"})
+    background_image_content: str = soup.find("div", {"class": "staff-pic"})["style"]
+    _, profile_picture, _ = re.split("\(|\)", background_image_content)
+    name: str = soup.find("div", {"class": "staff-name"}).get_text()
+    title: str = soup.find("div", {"class": "staff-title"}).get_text()
 
-    # test = staff_main_info.find("div", {"class": "staff-pic"})
-    return test
+    before_intro_tag: NavigableString = soup.find("div", {"class": "staff-heading"})
+    intro_tag = before_intro_tag.find_next_sibling()
+    join_date = intro_tag.find("strong").get_text()
 
-    return staff_main_info
-
-
-# def get_ogp_product_cost_component(
-#     ogp_api_product_info_response_soup: BeautifulSoup, cost_component: str
-# ) -> float:
-#     cost_component_tag = ogp_api_product_info_response_soup.find(
-#         "div", string=cost_component
-#     )
-#     if cost_component_tag is None:
-#         return 0
-#     cost_component_number_tag = cost_component_tag.find_previous_sibling("div")
-#     if cost_component_number_tag is None:
-#         return 0
-#     cost_component_html_value = cost_component_number_tag.get_text(strip=True)
-#     return int(cost_component_html_value[1:].replace(",", ""))
+    return StaffMember(profile_picture, name, title, join_date)
 
 
 ogp_api_people_info_response = get_ogp_api_people_info_response(PLACEHOLDER_URL)
