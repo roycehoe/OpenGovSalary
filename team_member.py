c27af5590382ea432e1c66ee45b44a6b4d58b870
roycehoe <royceinbox@hotmail.com>
2024-08-27 11:10:35 +0800

    refactor: handle errors properly in team member module to adhere to linting rules

--- team_member.py
+++ team_member.py
@@ -2,6 +2,7 @@ import re
 from dataclasses import dataclass
 from datetime import datetime
 from test import OGP_BASE_URL
+from typing import Optional
 
 import requests
 from bs4 import BeautifulSoup, NavigableString
@@ -14,7 +15,7 @@ class StaffInfo:
     profile_picture: str
     name: str
     title: str
-    join_date: datetime
+    join_date: Optional[datetime]
 
 
 def get_ogp_api_people_info_response(
@@ -27,7 +28,7 @@ def get_ogp_api_people_info_response(
         raise Exception  # To handle custom error here
 
 
-def get_profile_picture(ogp_api_people_info_response_soup: BeautifulSoup) -> str:
+def get_staff_profile_picture(ogp_api_people_info_response_soup: BeautifulSoup) -> str:
     background_image_content = ogp_api_people_info_response_soup.find(
         "div", {"class": "staff-pic"}
     )
@@ -49,18 +50,49 @@ def get_profile_picture(ogp_api_people_info_response_soup: BeautifulSoup) -> str
     return f"{OGP_BASE_URL}{profile_picture_subdirectory}"
 
 
+def _get_staff_name(ogp_api_people_info_response_soup: BeautifulSoup) -> str:
+    name = ogp_api_people_info_response_soup.find("div", {"class": "staff-name"})
+    if name is None:
+        return ""
+    return name.get_text()
+
+
+def _get_staff_title(ogp_api_people_info_response_soup: BeautifulSoup) -> str:
+    name = ogp_api_people_info_response_soup.find("div", {"class": "staff-title"})
+    if name is None:
+        return ""
+    return name.get_text()
+
+
+def _get_staff_join_date(
+    ogp_api_people_info_response_soup: BeautifulSoup,
+) -> Optional[datetime]:
+    before_intro_tag = ogp_api_people_info_response_soup.find(
+        "div", {"class": "staff-heading"}
+    )
+    if before_intro_tag is None:
+        return None
+    intro_tag = before_intro_tag.find_next_sibling()
+    if intro_tag is None:
+        return None
+    join_date_tag = intro_tag.find("strong")
+    if join_date_tag is None:
+        return None
+    if isinstance(join_date_tag, int):
+        return None
+
+    join_date_content = join_date_tag.get_text()
+    join_date = datetime.strptime(join_date_content, "%B %d, %Y")
+    return join_date
+
+
 def get_staff_info(ogp_api_people_info_response: str) -> StaffInfo:
     soup = BeautifulSoup(ogp_api_people_info_response, features="html.parser")
 
-    profile_picture = get_profile_picture(soup)
-
-    name: str = soup.find("div", {"class": "staff-name"}).get_text()
-    title: str = soup.find("div", {"class": "staff-title"}).get_text()
-
-    before_intro_tag: NavigableString = soup.find("div", {"class": "staff-heading"})
-    intro_tag = before_intro_tag.find_next_sibling()
-    join_date_content = intro_tag.find("strong").get_text()
-    join_date = datetime.strptime(join_date_content, "%B %d, %Y")
+    profile_picture = get_staff_profile_picture(soup)
+    name = _get_staff_name(soup)
+    title = _get_staff_title(soup)
+    join_date = _get_staff_join_date(soup)
 
     return StaffInfo(profile_picture, name, title, join_date)
 
@@ -68,7 +100,7 @@ def get_staff_info(ogp_api_people_info_response: str) -> StaffInfo:
 def main():
     ogp_api_people_info_response = get_ogp_api_people_info_response(PLACEHOLDER_URL)
     soup = BeautifulSoup(ogp_api_people_info_response, features="html.parser")
-    print(get_profile_picture(soup))
+    print(get_staff_profile_picture(soup))
 
 
 main()
