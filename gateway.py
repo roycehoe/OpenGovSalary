7b417002797b7c0bd0940b75cb3723f813770368
roycehoe <royceinbox@hotmail.com>
2024-08-27 12:22:50 +0800

    chore: remove unused functions in gateway and store api calls in gateway

--- gateway.py
+++ gateway.py
@@ -1,68 +1,31 @@
 import requests
 
-from models import OgpApiProductMembersResponse, OgpProductCost, OgpRepo
-
+OGP_PRODUCTS_URL = "https://products.open.gov.sg/"
 OGP_BASE_URL = "https://open.gov.sg/"
-OGP_REPOS_URL = "https://products.open.gov.sg/api/repos"
-OGP_PRODUCTS_BASE_URL = "https://products.open.gov.sg/"
 DEFAULT_START_DATE = "2023-07-01"
 
-
-def get_ogp_api_all_repos_response(
-    url: str = OGP_REPOS_URL,
-) -> list[OgpRepo]:
-    try:
-        ogp_repos_response = requests.get(url)
-        return [OgpRepo(**ogp_repo) for ogp_repo in ogp_repos_response.json()]
-    except Exception:
-        raise Exception  # To handle custom error here
+SALARY_HTML_TAG = "Salary"
+INFRASTRUCTURE_HTML_TAG = "Infrastructure"
+CORPORATE_OVERHEAD_HTML_TAG = "Corporate Overhead"
+EQUIPMENT_SOFTWARE_AND_OFFICE_HTML_TAG = "Equipment, Software & Office"
+OTHERS_HTML_TAG = "Others"
 
 
-def _get_ogp_product_cost_url(
-    product_name: str,
-    start_date: str = DEFAULT_START_DATE,
-    ogp_base_url: str = OGP_PRODUCTS_BASE_URL,
-):
-    return f"{ogp_base_url}{product_name}/api/costs?startDate={start_date}"
-
-
-def get_ogp_api_product_cost_response(product_path: str) -> OgpProductCost:
-    url = _get_ogp_product_cost_url(product_path)
-    try:
-        ogp_product_cost_response = requests.get(url)
-        return OgpProductCost(**ogp_product_cost_response.json())
-    except Exception:
-        raise Exception  # To handle custom error here
-
-
-def _get_ogp_api_product_members_url(
-    product_name: str,
-    start_date: str = DEFAULT_START_DATE,
-    ogp_base_url: str = OGP_PRODUCTS_BASE_URL,
-):
-    return f"{ogp_base_url}{product_name}/api/members?startDate={start_date}"
-
-
-def get_ogp_api_product_members_response(
-    product_path: str,
-) -> list[OgpApiProductMembersResponse]:
-    url = _get_ogp_api_product_members_url(product_path)
-    try:
-        ogp_api_product_members_response = requests.get(url)
-        return [
-            OgpApiProductMembersResponse(**ogp_api_product_member)
-            for ogp_api_product_member in ogp_api_product_members_response.json()
-        ]
-    except Exception:
-        raise Exception  # To handle custom error here
-
-
-def get_ogp_api_people_response(
-    id: str,
+def get_ogp_api_products_response(
+    url: str = OGP_PRODUCTS_URL,
 ) -> str:
-    url = f"{OGP_BASE_URL}people/{id}"
     try:
-        ogp_api_people_response = requests.get(url)
-        return ogp_api_people_response.text
+        ogp_products_response = requests.get(url, timeout=5)
+        return ogp_products_response.text
+    except Exception:
+        raise Exception  # To handle custom error here
+
+
+def get_ogp_api_product_info_response(
+    url: str,
+) -> str:
+    try:
+        ogp_product_info_response = requests.get(url, timeout=5)
+        return ogp_product_info_response.text
     except Exception:
         raise Exception  # To handle custom error here
--- test.py
+++ test.py
@@ -1,40 +1,19 @@
 from typing import Optional
 from unicodedata import numeric
 
-import requests
 from bs4 import BeautifulSoup, Tag
 
+from gateway import (
+    CORPORATE_OVERHEAD_HTML_TAG,
+    EQUIPMENT_SOFTWARE_AND_OFFICE_HTML_TAG,
+    INFRASTRUCTURE_HTML_TAG,
+    OGP_PRODUCTS_URL,
+    OTHERS_HTML_TAG,
+    SALARY_HTML_TAG,
+    get_ogp_api_product_info_response,
+    get_ogp_api_products_response,
+)
 from models import OgpProduct, OgpProductCost, OgpProductMember
-
-OGP_PRODUCTS_URL = "https://products.open.gov.sg/"
-OGP_BASE_URL = "https://open.gov.sg/"
-DEFAULT_START_DATE = "2023-07-01"
-
-SALARY_HTML_TAG = "Salary"
-INFRASTRUCTURE_HTML_TAG = "Infrastructure"
-CORPORATE_OVERHEAD_HTML_TAG = "Corporate Overhead"
-EQUIPMENT_SOFTWARE_AND_OFFICE_HTML_TAG = "Equipment, Software & Office"
-OTHERS_HTML_TAG = "Others"
-
-
-def get_ogp_api_products_response(
-    url: str = OGP_PRODUCTS_URL,
-) -> str:
-    try:
-        ogp_products_response = requests.get(url, timeout=5)
-        return ogp_products_response.text
-    except Exception:
-        raise Exception  # To handle custom error here
-
-
-def get_ogp_api_product_info_response(
-    url: str,
-) -> str:
-    try:
-        ogp_product_info_response = requests.get(url, timeout=5)
-        return ogp_product_info_response.text
-    except Exception:
-        raise Exception  # To handle custom error here
 
 
 def get_ogp_repo_tags(a_tags: list[Tag]) -> list[Tag]:
